5f9f6ad0bd434c5ae94c460d47646ada6195b389
Zakary Johnson <zakary.johnson575@gmail.com>
2024-12-18 13:15:03 -0600

    added a search bar and functioinallity

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,60 +1,61 @@
+const form = document.querySelector("#form");
+const cityInput = document.querySelector("#search");
+
 
 function fetchCityWeather(query) {
-    const apiKey = "6b72207fdbe6c16dfd1499cbda3aa797";
-    const city = encodeURI(query)
+  const apiKey = "6b72207fdbe6c16dfd1499cbda3aa797";
+  const city = encodeURI(query);
 
-    const url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
+  const url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
 
-    return fetch(url)
-      .then(function (response) {
-        if (!response.ok) {
-          return [];
-        }
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data)
-        console.log(data[0].lat)
-        console.log(data[0].lon)
-        fetchWeather(data)
-      })
-      .catch(function (error) {
+  return fetch(url)
+    .then(function (response) {
+      if (!response.ok) {
         return [];
-      });
-    }
-
-
-
-
-
-
-
-
-
+      }
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      console.log(data[0].lat);
+      console.log(data[0].lon);
+      fetchWeather(data);
+    })
+    .catch(function (error) {
+      return [];
+    });
+}
 
 function fetchWeather(query) {
-    const apiKey = "6b72207fdbe6c16dfd1499cbda3aa797";
-    const lat = query[0].lat;
-    const lon = query[0].lon;
+  const apiKey = "6b72207fdbe6c16dfd1499cbda3aa797";
+  const lat = query[0].lat;
+  const lon = query[0].lon;
 
+  const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
-    const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-
-    return fetch(url)
-      .then(function (response) {
-        if (!response.ok) {
-          return [];
-        }
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data)
-        return dataS || [];
-      })
-      .catch(function (error) {
+  return fetch(url)
+    .then(function (response) {
+      if (!response.ok) {
         return [];
-      });
-    }
+      }
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      return dataS || [];
+    })
+    .catch(function (error) {
+      return [];
+    });
+}
 
-    console.log('hi')
-fetchCityCoords('nashville')
+
+function handleSearch(event){
+event.preventDefault()
+console.log(cityInput.value)
+fetchCityWeather(cityInput.value)
+}
+
+form.addEventListener('submit',handleSearch);
+
+
